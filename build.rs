f79fcddb61eb1f07b41fdc24671680dfc0489f5f
Vadim <angryvadik@gmail.com>
2024-01-06 13:46:46 +0100

    build: shitty build script

---
+++ build.rs
@@ -0,0 +1,51 @@
+use std::{env, fs, path::PathBuf, str::FromStr};
+
+use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions};
+
+const APP_NAME: &str = "media_server";
+
+fn init_prod_storage() -> PathBuf {
+    let home = PathBuf::from(env::var("HOME").unwrap_or("~".into()));
+    let mut store_path = home.join(".local");
+    store_path.push("share");
+    store_path.push(APP_NAME);
+    store_path
+}
+
+fn init_debug_storage() -> PathBuf {
+    PathBuf::from(".").canonicalize().unwrap()
+}
+
+#[tokio::main]
+async fn main() {
+    let is_prod = !cfg!(debug_assertions);
+
+    let store_path = if is_prod {
+        init_prod_storage()
+    } else {
+        init_debug_storage()
+    };
+    let db_folder = store_path.join("db");
+
+    fs::create_dir_all(&db_folder).unwrap();
+    fs::create_dir_all(store_path.join("resources")).unwrap();
+    let db_path = db_folder.join("database.sqlite");
+
+    fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&db_path)
+        .unwrap();
+    let db_url = format!("sqlite://{}", db_path.to_string_lossy().to_string());
+    let mut connection = SqliteConnectOptions::from_str(&db_url)
+        .unwrap()
+        .connect()
+        .await
+        .unwrap();
+    let init_query = fs::read_to_string("init.sql").unwrap();
+    sqlx::query(&init_query)
+        .execute(&mut connection)
+        .await
+        .unwrap();
+}
